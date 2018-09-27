ec0d671b0f0555291c197cf94ad964b9cbef4d62
Rhys Rustad-Elliott <me@rhysre.net>
2018-09-27 18:00:05 +0100

    Make channel configurable, improve latest post detection

--- piazza_squatting_bot.py
+++ piazza_squatting_bot.py
@@ -7,6 +7,7 @@ import yaml
 from pypandoc import convert_text
 
 webhook_url = ''
+channel = ''
 classes = []
 
 
@@ -27,7 +28,7 @@ class PiazzaClass:
         self.subscribers = subscribers
         self.icon_emoji = icon_emoji
         self.bot_name = bot_name
-        self.last_notified_post = None
+        self.last_notified_post = network.get_statistics()['total']['questions']
 
     def notify(self, subject, question_url, body):
         '''
@@ -36,7 +37,7 @@ class PiazzaClass:
         '''
         subscriber_mentions = ' '.join(['<@%s>' % s for s in self.subscribers])
         requests.post(webhook_url, {'payload': json.dumps({
-            'channel': '#general',
+            'channel': channel,
             'username': self.bot_name,
             'icon_emoji': self.icon_emoji,
             'text': '%s New post in %s Piazza' %
@@ -55,8 +56,8 @@ def get_config():
     config.yml
     '''
     try:
-        config_file=open('config.yml', 'r')
-        yaml_config=yaml.load(config_file)
+        config_file = open('config.yml', 'r')
+        yaml_config = yaml.load(config_file)
         config_file.close()
     except IOError:
         sys.exit('config.yml was not found or error parsing YAML')
@@ -70,10 +71,10 @@ def parse_class_configs(piazza):
     the global webhook_url. Needs a Piazza object to get the network object
     for each class.
     '''
-    yaml_config=get_config()
+    yaml_config = get_config()
 
     for config in yaml_config['class_configs']:
-        network=piazza.network(config['network_id'])
+        network = piazza.network(config['network_id'])
         classes.append(
             PiazzaClass(
                 config['name'],
@@ -89,9 +90,13 @@ def parse_global_config():
     Parses and sets global (ie. not related to a particular class)
     configuration data from config.yml
     '''
-    yaml_config=get_config()
+    yaml_config = get_config()
+
     global webhook_url
-    webhook_url=yaml_config['webhook_url']
+    webhook_url = yaml_config['webhook_url']
+
+    global channel
+    channel = yaml_config['channel']
 
 
 def try_login():
@@ -99,14 +104,14 @@ def try_login():
     Tries to login to Piazza using the username/password specified in
     config.yml
     '''
-    yaml_config=get_config()
+    yaml_config = get_config()
 
     print 'Attempting to login to Piazza as %s.....' % yaml_config['username'],
-    piazza=Piazza()
+    piazza = Piazza()
 
     try:
         piazza.user_login(
-            email=yaml_config['username'], password=yaml_config['password'])
+            email = yaml_config['username'], password = yaml_config['password'])
     except BaseException:
         print 'failed, check username/password'
 
@@ -119,7 +124,7 @@ def announce_online():
     Sends a message to #general stating the bot is starting
     '''
     requests.post(webhook_url, {'payload': json.dumps({
-        'channel': '#general',
+        'channel': channel,
         'text': 'Piazza squatting bot is starting. Monitoring classes: %s' %
                 ', '.join([cl.name for cl in classes])
     })})
@@ -132,31 +137,22 @@ def poll_classes():
     '''
     while True:
         for cl in classes:
-            # No latest post yet (bot has just started), set top post as latest
-            if cl.last_notified_post is None:
-                latest_post_in_class=next(cl.network.iter_all_posts(limit=1))
-                cl.last_notified_post=latest_post_in_class['nr']
-                continue
-
             # Grab all new posts
-            to_notify=[]
+            to_notify = []
             for post in cl.network.iter_all_posts(limit=5):
-                if post['bucket_name'] == 'Pinned':
-                    continue
-
-                if post['nr'] > cl.last_notified_post:
+                if post['nr'] > cl.last_notified_post and post['type'] == 'question':
                     to_notify.append(post)
 
             # Send a notification for each one and update cl.last_notified_post
             for post in to_notify:
-                question_url='https://piazza.com/class/%s?cid=%s' % (
+                question_url = 'https://piazza.com/class/%s?cid=%s' % (
                     cl.network_id, post['nr'])
 
                 # 0xA0 = Non breaking space, found in Piazza HTML, must be
                 # converted to regular space
-                subject=convert_text(post['history'][0]['subject'].replace(
+                subject = convert_text(post['history'][0]['subject'].replace(
                     u'\xa0', ' '), 'plain', format='html')
-                body=convert_text(post['history'][0]['content'].replace(
+                body = convert_text(post['history'][0]['content'].replace(
                     u'\xa0', ' '), 'plain', format='html')
                 cl.notify(subject, question_url, body)
 
@@ -166,7 +162,7 @@ def poll_classes():
         time.sleep(10)
 
 
-piazza=try_login()
+piazza = try_login()
 parse_global_config()
 parse_class_configs(piazza)
 announce_online()
