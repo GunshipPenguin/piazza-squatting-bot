46d619d0d47f906ebe36155f5e216f416c22954f
Rhys Rustad-Elliott <me@rhysre.net>
2018-09-27 13:52:37 +0100

    Totally rework bot
    - Store config information in config.yml
    - Add multiple class polling
    - Break up / clean up ugly code

--- piazza_squatting_bot.py
+++ piazza_squatting_bot.py
@@ -2,80 +2,165 @@ from piazza_api import Piazza
 import time
 import requests
 import json
-import argparse
+import sys
+import yaml
 from pypandoc import convert_text
 
-# Email/Password to login to Piazza with
-EMAIL = ''
-PASSWORD = ''
+webhook_url = ''
+classes = []
 
-# Slack webhook URL for notifications
-WEBHOOK_URL = ''
 
-# Piazza class name and network ID
-CLASS_NAME = ''
-NETWORK_ID = ''
+class PiazzaClass:
+    '''
+    Represents a class on Piazza.
+    '''
+    def __init__(self, name, network, subscribers, icon_emoji, bot_name):
+        '''(str, piazza_api.Network, list[str], str, str)
+        Create a new Piazza Class with the given class name, network id, list
+        of Slack handles to receive notifications, bot emoji and bot name.
+        '''
+        self.name = name
+        self.network = network
+        self.subscribers = subscribers
+        self.icon_emoji = icon_emoji
+        self.bot_name = bot_name
+        self.last_notified_post = None
 
-# Bot name and emoji to use as bot icon
-BOT_NAME = ''
-ICON_EMOJI = ''
-
-while True:
-    print 'Logging into Piazza...'
-    try:
-        p = Piazza()
-        p.user_login(email=EMAIL, password=PASSWORD)
-
-        requests.post(WEBHOOK_URL, {'payload': json.dumps({
+    def notify(self, subject, question_url, body):
+        '''
+        Notify's this classe's subscribers with the given subject, a link to the
+        given question url, and question body.
+        '''
+        requests.post(webhook_url, {'payload': json.dumps({
             'channel': '#general',
-            'username': BOT_NAME,
-            'text': '%s is starting' % BOT_NAME,
-            'icon_emoji': ICON_EMOJI,
+            'username': self.bot_name,
+            'text': 'New post in %s %s' % (self.name, ['@' + s for s in subscribers]),
+            'attachments': [{
+                "fallback": 'New post in %s: %s' % (self.name, question_url),
+                "title": subject,
+                "title_link": question_url,
+                "text": body,
+                "mrkdwn_in": ["text"]
+            }],
+            'icon_emoji': self.icon_emoji,
             'link_names': True
         })})
 
-        n = p.network(NETWORK_ID)
 
-        latest_post = None
-        next_latest_post = None
+def get_config():
+    '''
+    Reads, parses and returns a dictionary containing the contents of
+    config.yml
+    '''
+    try:
+        config_file = open('config.yml', 'r')
+        yaml_config = yaml.load(config_file)
+        config_file.close()
+    except IOError:
+        sys.exit('config.yml was not found or error parsing YAML')
 
-        while True:
-            for post in n.iter_all_posts(limit=5):
+    return yaml_config
+
+
+def parse_class_configs(piazza):
+    '''
+    Initializes the bot with the class configs found in config.yml and sets
+    the global webhook_url. Needs a Piazza object to get the network object
+    for each class.
+    '''
+    yaml_config = get_config()
+
+    for config in yaml_config['class_configs']:
+        network = piazza.network(config['network_id'])
+        classes.append(PiazzaClass(config['name'], network,
+                config['subscribers'], config['icon_emoji'], config['bot_name']))
+
+
+def parse_global_config():
+    '''
+    Parses and sets global (ie. not related to a particular class) configuration
+    data from config.yml
+    '''
+    yaml_config = get_config()
+    global webhook_url
+    webhook_url = yaml_config['webhook_url']
+
+
+def try_login():
+    '''
+    Tries to login to Piazza using the username/password specified in config.yml
+    '''
+    yaml_config = get_config()
+
+    print 'Attempting to login to Piazza as %s.....' % yaml_config['username'],
+    piazza = Piazza()
+
+    try:
+        piazza.user_login(email=yaml_config['username'], password=yaml_config['password'])
+    except:
+        print 'failed, check username/password'
+
+    print 'success'
+    return piazza
+
+
+def announce_online():
+    '''
+    Sends a message to #general stating the bot is starting
+    '''
+    requests.post(webhook_url, {'payload': json.dumps({
+        'channel': '#general',
+        'text': 'Piazza squatting bot is starting. Monitoring classes: %s' % ', '.join([cl.name for cl in classes])
+    })})
+
+
+def poll_classes():
+    '''
+    Loops until program is terminated, polling Piazza periodically for new posts
+    and notifying all subscribers of that class.
+    '''
+    while True:
+        for cl in classes:
+            # No latest post yet (bot has just started), set top post as latest
+            if cl.last_notified_post is None:
+                latest_post = next(cl.network.iter_all_posts(limit=1))
+                cl.last_notified_post = latest_post['nr']
+                continue
+
+            # Grab all new posts
+            to_notify = []
+            for post in cl.network.iter_all_posts(limit=5):
                 if post['bucket_name'] == 'Pinned':
                     continue
-                nr = post['nr']
-                print nr, latest_post
-                if latest_post is None:
-                    next_latest_post = nr
-                    break
-                if nr > latest_post:
-                    question_url = 'https://piazza.com/class/%s?cid=%s' % (
-                        NETWORK_ID, nr)
-                    subject = convert_text(post['history'][0]['subject'].replace(
-                        u'\xa0', ' '), 'markdown', format='html')
-                    body = convert_text(post['history'][0]['content'].replace(
-                        u'\xa0', ' '), 'markdown', format='html')
-                    requests.post(WEBHOOK_URL, {'payload': json.dumps({
-                        'channel': '#general',
-                        'username': BOT_NAME,
-                                    'text': '@channel New post in %s :crystal_ball: :crystal_ball: :crystal_ball:' % CLASS_NAME,
-                                    'attachments': [{
-                                        "fallback": 'New post in %s: %s' % (CLASS_NAME, question_url),
-                                        "title": subject,
-                                        "title_link": question_url,
-                                        "text": body,
-                                        "mrkdwn_in": ["text"]
-                                    }],
-                        'icon_emoji': ICON_EMOJI,
-                        'link_names': True
-                    })})
-                    next_latest_post = max(next_latest_post, nr)
 
-            latest_post = next_latest_post
-            time.sleep(10)
-    except KeyboardInterrupt:
-        break
-    except:
-        import traceback
-        traceback.print_exc()
-        print 'Restarting app...'
+                if post['nr'] > cl.last_notified_post:
+                    to_notify.append(post)
+
+            # Send a notification for each one and update cl.last_notified_post
+            for post in to_notify:
+                question_url = 'https://piazza.com/class/%s?cid=%s' % (network_id, nr)
+                subject = convert_text(post['history'][0]['subject'].replace(
+                    u'\xa0', ' '), 'markdown', format='html')
+                body = convert_text(post['history'][0]['content'].replace(
+                    u'\xa0', ' '), 'markdown', format='html')
+                cl.notify(subject, question_url, body)
+
+                if cl.last_notified_post < post['nr']:
+                    cl.last_notified_post = post['nr']
+
+        time.sleep(10)
+
+
+piazza = try_login()
+parse_global_config()
+parse_class_configs(piazza)
+announce_online()
+
+try:
+    poll_classes()
+except KeyboardInterrupt:
+    sys.exit('Caught KeyboardInterrupt')
+except:
+    import traceback
+    traceback.print_exc()
+    print 'Unknown error occured'
